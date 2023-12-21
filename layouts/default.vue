533e24f69cc12ed6df5724c3d4ce90976cedd616
Axel <axelsorensen.dev@gmail.com>
2023-12-21 19:25:40 +0100

    fixed h-screen cards

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-screen grid grid-rows-[auto,1fr] overflow-hidden">
     <SiteHeader />
-    <div class="h-full overflow-scroll flex p-8">
+    <div class="h-full overflow-scroll flex">
       <slot />
     </div>
   </div>
