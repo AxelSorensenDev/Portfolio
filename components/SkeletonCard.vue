e7e15df375a7097c4a19aaff8de91c33ab01e369
Axel <axelsorensenwork@gmail.com>
2024-03-09 16:22:40 +0100

    updated skeleton card

--- components/SkeletonCard.vue
+++ components/SkeletonCard.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="rounded-lg overflow-hidden bg-white hover:bg-gray-50">
+  <div class="rounded-lg overflow-hidden bg-white hover:bg-gray-50 h-[314px]">
     <div class="w-full h-[150px] bg-gray-200 animate-pulse"></div>
     <div class="px-6 py-4">
-      <div class="font-bold h-[1.5em] mb-2 animate-pulse text-ellipsis line-clamp-1 bg-gray-100"></div>
-      <div v-for="_ in 2" class="text-gray-700 h-[1em] text-sm line-clamp-2 bg-gray-100 animate-pulse mb-1">
+      <div class="font-bold h-6 mb-4 animate-pulse text-ellipsis line-clamp-1 bg-gray-100"></div>
+      <div v-for="_ in 2" class="text-gray-700 h-3 mb-2 bg-gray-100 animate-pulse">
       </div>
     </div>
-    <div class="px-6 pt-4 pb-2 truncate text-ellipsis">
+    <div class="px-6 pt-3 pb-2 truncate text-ellipsis">
       <span v-for="tag in 3"
-        class="inline-block bg-gray-100 rounded-full px-3 py-1 text-sm font-medium text-gray-700 mr-2 mb-2 h-6 w-16 animate-pulse"></span>
+        class="inline-block bg-gray-100 rounded-full px-3 py-1 text-sm font-medium text-gray-700 mr-2 h-6 w-16 animate-pulse"></span>
     </div>
   </div>
 </template>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="">
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-5">
-      <div class=" drop-shadow-md" v-if="pending" v-for="_ in 10">
+      <div class=" drop-shadow-md" v-if="pending" v-for="_ in 2">
         <SkeletonCard />
       </div>
       <div v-else class=" drop-shadow-md" v-for="post in posts">
