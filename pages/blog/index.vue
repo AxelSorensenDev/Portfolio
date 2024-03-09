0c4f521aba05d6851abda9929323b7806d5d0454
Axel <axelsorensenwork@gmail.com>
2024-03-09 16:07:46 +0100

    added shadow to skeleton card

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="">
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-5">
-      <div v-if="pending" v-for="_ in 10">
+      <div class=" drop-shadow-md" v-if="pending" v-for="_ in 10">
         <SkeletonCard />
       </div>
-      <div class=" drop-shadow-md" v-for="post in posts">
+      <div v-else class=" drop-shadow-md" v-for="post in posts">
         <NuxtLink :to="`blog/${post.id}`">
           <LazyCard :post="post" />
         </NuxtLink>
