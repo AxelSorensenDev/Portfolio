40d0f2d2f1a2df3aa92c87f99cef97506f0ec8f1
Axel <axelsorensenwork@gmail.com>
2024-03-09 16:41:29 +0100

    added more skeleton cards to blog screen

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="">
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-5">
-      <div class=" drop-shadow-md" v-if="pending" v-for="_ in 2">
+      <div class=" drop-shadow-md" v-if="pending" v-for="_ in 10">
         <SkeletonCard />
       </div>
-      <div v-else class=" drop-shadow-md" v-for="post in posts">
+      <div v-if="loaded" class=" drop-shadow-md" v-for="post in posts">
         <NuxtLink :to="`blog/${post.id}`">
-          <LazyCard :post="post" />
+          <Card :post="post" />
         </NuxtLink>
       </div>
 
@@ -15,6 +15,7 @@
 </template>
 
 <script setup>
+const loaded = ref(false)
 // Fetch products
 const { data: posts, pending } = useLazyAsyncData('posts', () => queryContent('/blog').find())
 
